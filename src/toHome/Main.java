7412293bae35b5bbf4791cbe7262d1a7136cee7e
Michael Christian Buratti <mburatti@users.noreply.github.com>
2021-03-12 12:14:07 -0500

    Deletes correction clean code badnames

--- src/toHome/Main.java
+++ src/toHome/Main.java
@@ -5,49 +5,78 @@ import java.io.File;
 
 public class Main {
 
-    private static String[] starts = {	"C:\\Users\\mbura\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs",
-    									"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"	};
+    private static final String[] starts = {"C:\\Users\\mbura\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs",
+            "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"};
+
+    private static String currentRoot;
+    private static String source;
+    private static String destiny;
 
     public static void main(String[] args) {
         JavaElevator.elevate(args);
-    	System.out.println("toHome");
+        System.out.println("toHome");
         System.out.println("toHome VER 1.2");
-        
-        for(String start : starts)
-        	folder(start);
-       
+
+        for (String start : starts) {
+            currentRoot = start;
+            verifyFolder(start);
+        }
+
+
         System.out.println("Execution Terminated");
     }
 
-    private static int folder(String path) {
+    /**
+     * verifyFolder.
+     *
+     * @param path
+     * @return true if the folder is empty
+     */
+    private static boolean verifyFolder(String path) {
         File folder = new File(path);
 
-        System.out.println("Openning Directory: " + path);
-        File[] listOfFiles = folder.listFiles(); 
-        
-        folder.listFiles();
-        
+        File[] listOfFiles = folder.listFiles();
+
         for (File file : listOfFiles)
             if (file.isFile()) {
-            	if (!file.getName().equals(path + "\\" + file.getName())) {
-            		System.out.println(file.getAbsolutePath() + " MOVE to: " + path + "\\" + file.getName());
-            		file.renameTo(new File(path + "\\" + file.getName()));
-            	}
-            } else if (file.isDirectory()) {
-                System.out.println("Directory: " + file.getAbsolutePath());
-                if (folder(file.getAbsolutePath()) == 1) {
-                	file.delete();
+                if (BlackList.contains(file.getName())) {
+                    deleteFile(file);
+                } else {
+                    source = file.getAbsolutePath();
+                    destiny = getDestinyFileName(file);
+
+                    if (!source.equalsIgnoreCase(destiny)) {
+                        System.out.println(file.getAbsolutePath() + " MOVE to: " + getDestinyFileName(file));
+                        file.renameTo(new File(getDestinyFileName(file)));
+                    }
                 }
             }
 
-        return FolderIsVoid(folder);
+        for (File file : listOfFiles)
+            if (file.isDirectory()) {
+                System.out.println("Directory: " + file.getAbsolutePath());
+                if (verifyFolder(file.getAbsolutePath())) {
+                    file.delete();
+                }
+            }
+
+        return isFolderVoid(folder);
     }
-    
-    static int FolderIsVoid(File folder) {        
+
+    private static void deleteFile(File file) {
+        System.out.println("DELETE " + file.getName());
+        file.deleteOnExit();
+    }
+
+    private static String getDestinyFileName(File file) {
+        return currentRoot + "\\" + file.getName();
+    }
+
+    static boolean isFolderVoid(File folder) {
         if (folder.listFiles().length == 0) {
-            folder.delete();
-            return 1;
+            deleteFile(folder);
+            return true;
         }
-        return 0;
+        return false;
     }
 }
